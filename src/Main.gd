3552bd45f74c63eb086e39b5be410f008e404281
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-05-30 18:31:19 -0300

    Win screen

--- src/Main.gd
+++ src/Main.gd
@@ -5,13 +5,19 @@ onready var camera_animation = $Pilot/Camera2D/AnimationPlayer
 onready var objective_label = $CanvasLayer/UI/Objective
 onready var animation = $AnimationPlayer
 var building_count = 10
+var kaiju_count = 4
 
 func _ready():
 	building_count = get_node("Buildings").get_child_count()
 	update_objetive()
 	
-	for building in get_node("Buildings").get_children():
-		building.connect("death", self, "_on_Building_death")
+	for node in get_node("Buildings").get_children():
+		node.connect("death", self, "_on_Building_death")
+	
+	kaiju_count = get_node("Kaijus").get_child_count()
+	
+	for node in get_node("Kaijus").get_children():
+		node.connect("death", self, "_on_Kaiju_death")
 
 
 func update_objetive():
@@ -26,12 +32,21 @@ func _on_Building_death():
 		update_objetive()
 
 
+func _on_Kaiju_death():
+	kaiju_count -= 1
+	if kaiju_count == 1 and get_node("Kaijus").get_children()[0].possessed:
+		animation.play("end_good")
+
+
 func _on_Pilot_mounted():
 	camera_animation.play("mount_zoom")
 
 func _on_Pilot_unmounted():
 	camera_animation.play_backwards("mount_zoom")
 
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "end":
-		var _status = get_tree().change_scene("res://src/GameOver.tscn")
+		var _status1 = get_tree().change_scene("res://src/GameOver.tscn")
+	if anim_name == "end_good":
+		var _status2 = get_tree().change_scene("res://src/YouWin.tscn")
--- src/kaiju/Jupiter.gd
+++ src/kaiju/Jupiter.gd
@@ -8,10 +8,12 @@ onready var viewing_area = $Flip/ViewingArea
 onready var mount_point = $Flip/MountArea
 onready var state_machine = $StateMachine
 
-
 var seeking_enemy = false
+
+signal death
 
 
 func _on_Life_death():
 	mount_point.call_unmount()
+	emit_signal("death")
 	queue_free()
